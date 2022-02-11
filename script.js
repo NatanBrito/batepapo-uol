d3ab776efa2656243a0de316ec9e7207c08c5646
Natan Brito_3 <natanismaelbrito@gmail.com>
2022-02-11 15:36:39 -0300

    inicio

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let enviandoNome = {
     name:nomeUsuario
 }
 console.log(enviandoNome);
+
+
+function addMessages (){
+    const containerMessages= document.querySelector(".allMessages")
+    messageHtml= ` 
+    <div class="message">
+    <img src="./img/logo 1.png">
+    <img src="./img/logo 1.png">
+</div>
+`
+containerMessages.innerHTML+= messageHtml;
+}
+console.log("oi");
