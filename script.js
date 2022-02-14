ce044160470b9b9006123623f2e6d34c1986faa4
Natan Brito_3 <natanismaelbrito@gmail.com>
2022-02-14 02:01:12 -0300

    feito as cores de mensagens e o scroll automatico

--- script.js
+++ script.js
@@ -1,56 +1,94 @@
-let nomeUsuario=prompt("digite seu nickname");
-let enviandoNome = {
-    name:nomeUsuario
-}
 let data;
+let enviandoNome;
+let testinhoMarotinho;
 
-console.log(enviandoNome);
+validandoNome();
+
+function validandoNome() {
+
+    let nomeUsuario = prompt("digite seu nickname");
+    enviandoNome = {
+        name: nomeUsuario
+    }
+
+    let sendName = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants ", enviandoNome)
+    sendName.then(sucess)
+    sendName.catch(fail)
+
+    function sucess() {
+        alert("Belo apelido");
+        
+    }
+    function fail() {
+        const nome = prompt("Digite outro nome pois este já está em uso");
+        let nomeDado = {
+            name: nome
+        };
+        const sendName = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", nomeDado);
+        sendName.then(sucess);
+        sendName.catch(fail);
+    }
+
+}
+// setInterval(timerConexao,3000)
+
+//   timerConexao(){
+    
+
+//   }
+
+
+
 const testRequest = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 testRequest.then(infoData);
 
-function infoData(promisse){
-  data= promisse.data
-    console.log(data.length);
-    spinMessage ();
+function infoData(promisse) {
+    data = promisse.data;
+    console.log(data.from);
+    addApiMessage();
 }
 
 
-let input= document.querySelector("input");
-inputMessage= input.value;
-function addMyMessages (){
+let input = document.querySelector("input");
+inputMessage = input.value;
+function addMyMessages() {
     sendMyMessage();
-    const containerMessages= document.querySelector(".allMessages")
-    messageHtml= ` 
+    const containerMessages = document.querySelector(".allMessages")
+    messageHtml = ` 
     <div class="message"><span>de <b>${enviandoNome.name}</b>: ${inputMessage}</span>
     </div>
     `
-    containerMessages.innerHTML+= messageHtml;
-    // messageHtml.scrollIntoView();
-inputMessage="";
+    containerMessages.innerHTML += messageHtml;
+    inputMessage = "";
 
 }
-function sendMyMessage(){
-    let input= document.querySelector("input");
-    inputMessage= input.value;
+function sendMyMessage() {
+    let input = document.querySelector("input");
+    inputMessage = input.value;
 }
-function addApiMessage (){
-    const add= document.querySelector(".allMessages")
-    messageApiHtml= ` 
-    <div class="message"><span> <b>(${data[i].time}) ${data[i].from}</b> para ${data[i].to}: ${data[i].text} </span>
-    </div>
-    `
-    add.innerHTML+= messageApiHtml;
 
-}
-function spinMessage (){
-    for(let i=0;i<data.length;i++){
-        const add= document.querySelector(".allMessages")
-        messageApiHtml= ` 
+function addApiMessage() {
+    for (let i = 0; i < data.length; i++) {
+        const add = document.querySelector(".allMessages")
+        if (data[i].type === "status") {
+            add.innerHTML += ` 
+        <div class="message status"><span> <b>(${data[i].time}) ${data[i].from}</b> para ${data[i].to}: ${data[i].text} </span>
+        </div>
+        `
+        }
+        else if (data[i].type === "private_message") {
+            add.innerHTML += ` 
+       <div class="message private"><span> <b>(${data[i].time}) ${data[i].from}</b> para ${data[i].to}: ${data[i].text} </span>
+       </div>
+       `
+
+        }
+        else {
+            add.innerHTML += ` 
         <div class="message"><span> <b>(${data[i].time}) ${data[i].from}</b> para ${data[i].to}: ${data[i].text} </span>
         </div>
         `
-        add.innerHTML+= messageApiHtml;
-    
+        }
     }
-
+    window.scrollTo(0, document.body.scrollHeight);
 }
